462268515892f024bbc38e379a58b9d0bf751146
Kacper Makowski <kacper.makowski1@gmail.com>
2021-06-21 21:01:17 +0200

    Create files to Goal project

---
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -0,0 +1,6 @@
+
+const CourseGoalItem = () => {
+
+}
+
+export default CourseGoalItem;
---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Button from '../../UI/Button';
+
+const CourseInput = props => {
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const goalInputChangeHandler = event =>{
+        setEnteredValue(event.target.value);
+    }
+
+    const formSubmitHandler = event =>{
+        event.preventDefault();
+        props.onAddGoal(enteredValue);
+    }
+
+    return(
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label>Course Goal</label>
+                <input type="text" onChange={goalInputChangeHandler}/>
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
+}
+
+export default CourseInput;
